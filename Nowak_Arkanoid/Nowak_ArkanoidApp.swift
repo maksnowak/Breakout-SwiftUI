c1215e1509ef0369f6b40f9e6afd6d59179c9beb
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-06-25 15:49:42 +0200

    Initial Commit

---
+++ Nowak_Arkanoid/Nowak_ArkanoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nowak_ArkanoidApp.swift
+//  Nowak_Arkanoid
+//
+//  Created by apios on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nowak_ArkanoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
